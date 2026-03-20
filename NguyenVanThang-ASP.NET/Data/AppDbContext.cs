0d3e5280ad7e7ff7b7d2b9d0e69b10c836227570
ngvanthang05 <nvt130205@example.com>
2026-03-20 16:07:59 +0700

    Add project files.

---
+++ NguyenVanThang-ASP.NET/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NguyenVanThang_ASP.NET.Models;
+
+namespace NguyenVanThang_ASP.NET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor này bắt buộc phải có để nhận Connection String từ Program.cs
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+                    {
+                    }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ NguyenVanThang-ASP.NET/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NguyenVanThang_ASP.NET.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string StudentCode { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+        public DateTime Birthday { get; set; }
+    }
+}
